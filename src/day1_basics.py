19dada0f4d82541e151f4d339239b5d2eca92ada
TimofeyAlexandrov <alexandrov.timofey@gmail.com>
2025-09-07 22:45:55 +0300

    lesson: day1_basics — variables and print

---
+++ src/day1_basics.py
@@ -0,0 +1,11 @@
+name="Timofey" #string
+age=31 #int
+height=1.86 #float
+is_student=True #bool
+numbers=[7,12,24] #list
+person={"name":"Timofey","age":31} #dict
+
+print(f"Привет, {name}!")
+print(f"Возраст: {age}")
+print(f"Рост: {height}")
+print(f"Учиться? {is_student}")
