71f8daec3c777d1e9d9a7ca5561978478df54718
TimofeyAlexandrov <alexandrov.timofey@gmail.com>
2025-09-12 21:59:11 +0300

    style: format sensor task with black

--- src/day5_task6_sensor.py
+++ src/day5_task6_sensor.py
@@ -1,10 +1,9 @@
-value=int(input("sensor value: "))
-if value==0:
+value = int(input("sensor value: "))
+if value == 0:
     print("OK")
-elif value==1:
+elif value == 1:
     print("WARN")
-elif value==2:
+elif value == 2:
     print("FAIL")
 else:
     print("UNKNOWN")
-
